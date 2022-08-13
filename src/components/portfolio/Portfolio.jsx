80d120b893e418b259b1fde800dfeda416bed636
hackwebdev <hackwebdev@gmail.com>
2022-08-13 16:24:42 +0800

    generating portfolio items from array using map method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,120 +7,76 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+// data
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https:github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Charts templates and infographics in figma',
+    github: 'https:github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Figma dashboard UI kit for data design web apss',
+    github: 'https:github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https:github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https:github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https:github.com',
+    demo: 'https://dribble.com',
+  },
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>
-              Github
-            </a>
-            <a
-              href='https://dribbble.com/shots/19072418-Shiprocket-Website-Redesign'
-              className='btn btn-primary'
-              target='_blank'
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG2} alt='' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>
-              Github
-            </a>
-            <a
-              href='https://dribbble.com/shots/19072418-Shiprocket-Website-Redesign'
-              className='btn btn-primary'
-              target='_blank'
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG3} alt='' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>
-              Github
-            </a>
-            <a
-              href='https://dribbble.com/shots/19072418-Shiprocket-Website-Redesign'
-              className='btn btn-primary'
-              target='_blank'
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG4} alt='' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>
-              Github
-            </a>
-            <a
-              href='https://dribbble.com/shots/19072418-Shiprocket-Website-Redesign'
-              className='btn btn-primary'
-              target='_blank'
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG5} alt='' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>
-              Github
-            </a>
-            <a
-              href='https://dribbble.com/shots/19072418-Shiprocket-Website-Redesign'
-              className='btn btn-primary'
-              target='_blank'
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG6} alt='' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>
-              Github
-            </a>
-            <a
-              href='https://dribbble.com/shots/19072418-Shiprocket-Website-Redesign'
-              className='btn btn-primary'
-              target='_blank'
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className='portfolio__item'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn' target='_blank'>
+                  Github
+                </a>
+                <a href={demo} className='btn btn-primary' target='_blank'>
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          )
+        })}
       </div>
     </section>
   )
