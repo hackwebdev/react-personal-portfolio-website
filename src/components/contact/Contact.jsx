c36c9ae3fb2252ae677d7b1eae3bcb5dcf1fbde1
hackwebdev <hackwebdev@gmail.com>
2022-08-13 23:12:06 +0800

    contact component css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
       <div className='container contact__container'>
         <div className='contact__options'>
           <article className='contact__option'>
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>tatakrgb@gmail.com</h5>
             <a href='mailto:tatakrgb@gmail.com' target='_blank'>
@@ -21,7 +21,7 @@ const Contact = () => {
             </a>
           </article>
           <article className='contact__option'>
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>tatakrgb@gmail.com</h5>
             <a href='https://m.me.com/tatakrgb' target='_blank'>
@@ -29,7 +29,7 @@ const Contact = () => {
             </a>
           </article>
           <article className='contact__option'>
-            <FaWhatsapp />
+            <FaWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <h5>+1234567</h5>
             <a href='https://api.whatsapp.com/send?phone+12345678'>
